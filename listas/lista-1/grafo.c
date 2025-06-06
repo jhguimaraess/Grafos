284bbb3f9fb33fe6f0a815c47dc32ab8235b2f7d
joao guimaraes <guimaraesjoao101@gmail.com>
2025-06-06 18:02:28 -0300

    update

--- listas/lista-1/grafo.c
+++ listas/lista-1/grafo.c
@@ -1,4 +1,7 @@
 #include "grafos.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 Grafo* CriaGrafo(int qtdVertices){
     Grafo* g = (Grafo*)malloc(sizeof(Grafo));
@@ -8,6 +11,12 @@ Grafo* CriaGrafo(int qtdVertices){
     g->Ma = (int**)malloc(qtdVertices * sizeof(int*));
     for(int i = 0; i < qtdVertices; i++){
         g->Ma[i] = (int*)malloc(qtdVertices * sizeof(int));
+    }
+
+    for(int i = 0; i < qtdVertices; i++){
+        for (int j = 0; j < qtdVertices; j++){
+            g->Ma[i][j] = 0;
+        }
     }
 
     return g;
@@ -24,7 +33,7 @@ void LiberaGrafo(Grafo *g){
 void imprimeGrafo(Grafo *g){
     for (int i = 0; i < g->NumVertices;i++){
         for (int j = 0; j < g->NumVertices; j++){
-            print("%d ", g->Ma[i][j]);
+            printf("%d ", g->Ma[i][j]);
         }
         printf("\n");
     }
@@ -39,7 +48,7 @@ void InsereAresta(Grafo* g, int V1, int V2){
 }
 
 void RemoveAresta(Grafo* g, int V1, int V2){
-    if (V1 >= 0 && V1 < g->NumVertices && V2 >= 0 && V2 < g->NumVertices && g->Ma[V1][V2] == 0){
+    if (V1 >= 0 && V1 < g->NumVertices && V2 >= 0 && V2 < g->NumVertices && g->Ma[V1][V2] == 1){
         g->Ma[V1][V2] = 0;
         g->Ma[V2][V1] = 0;
         g->NumArestas--;
@@ -48,7 +57,7 @@ void RemoveAresta(Grafo* g, int V1, int V2){
 
 void imprimeAdjacentes(int V, Grafo* g){
     if(V >= 0 && V < g->NumVertices){
-        prinf("Vertices adjacentes a %d: ", V);
+        printf("Vertices adjacentes a %d: ", V);
         for(int i = 0; i < g->NumVertices; i++){
             if(g->Ma[V][i] == 1){
                 printf("%d ", i);
@@ -59,20 +68,26 @@ void imprimeAdjacentes(int V, Grafo* g){
 }
 
 void InsereVertices(Grafo* g, int qtdVertices){
-    int novo_total = g->NumVertices + qtdVertices;
+    int antigo_total = g->NumVertices;
+    int novo_total = antigo_total + qtdVertices;
 
+    // Realoca o vetor de ponteiros (linhas da matriz)
     g->Ma = (int**)realloc(g->Ma, novo_total * sizeof(int*));
-    for(int i = 0; i < novo_total; i++){
-        g->Ma[i] = (int*)realloc(g->Ma[i], novo_total * sizeof(int));
 
-        for(int j = g->NumVertices; j < novo_total; j++){
+    // Realoca cada linha antiga para o novo número de colunas
+    for (int i = 0; i < antigo_total; i++) {
+        g->Ma[i] = (int*)realloc(g->Ma[i], novo_total * sizeof(int));
+        // Inicializa as novas colunas da linha realocada
+        for (int j = antigo_total; j < novo_total; j++) {
             g->Ma[i][j] = 0;
         }
     }
 
-    for(int i = g->NumVertices; i < novo_total; i++){
-        g->Ma[i] = (int*)calloc(novo_total, sizeof(int));
+    // Aloca as novas linhas
+    for (int i = antigo_total; i < novo_total; i++) {
+        g->Ma[i] = (int*)calloc(novo_total, sizeof(int));  // zera toda a linha
     }
 
+    // Atualiza o número total de vértices
     g->NumVertices = novo_total;
 }
--- listas/lista-1/main.c
+++ listas/lista-1/main.c
@@ -1,9 +1,33 @@
-#include "grafos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafo.c"
 
-int main(int argc, char const *argv[]){
+
+int main(){
 
     Grafo *g = CriaGrafo(5);
-    imprimeGrafo
+
+    imprimeGrafo(g);
+
+    InsereAresta(g, 2, 4);
+    InsereAresta(g, 1, 2);
+    InsereAresta(g, 1, 0);
+    InsereAresta(g, 1, 3);
+
+    printf("------------------------\n");
+    imprimeGrafo(g);
+
+    printf("------------------------\n");
+    RemoveAresta(g, 2, 4);
+    imprimeGrafo(g);
+
+    printf("------------------------\n");
+    imprimeAdjacentes(1, g);
+
+    printf("------------------------\n");
+    InsereVertices(g, 3);
+    imprimeGrafo(g);
+
 
     return 0;
 }
